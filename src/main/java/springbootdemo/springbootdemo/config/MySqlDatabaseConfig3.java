34f510148ddbca2ed11a6639e53855ba0f342fff
ashfaq-m <ashapak.makandar7@gmail.com>
2024-08-04 23:55:04 +0530

    Un-used imports removed

--- src/main/java/springbootdemo/springbootdemo/config/MySqlDatabaseConfig3.java
+++ src/main/java/springbootdemo/springbootdemo/config/MySqlDatabaseConfig3.java
@@ -1,6 +1,6 @@
 package springbootdemo.springbootdemo.config;
 
-import com.zaxxer.hikari.HikariDataSource;
+
 import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
@@ -8,13 +8,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import springbootdemo.springbootdemo.student.StudentEntity;
+
 
 import javax.sql.DataSource;
 
